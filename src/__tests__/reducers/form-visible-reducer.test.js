4cfb212255d65311fa6adf03d87b13608bf874cf
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 11:07:40 -0800

    Write and FAIL form reducer test for type toggle form

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,7 +1,11 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe('formVisibleReducer', () => {
-test('should return default state if no action type is recognized', () => {
+  test('should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, {type:'TOGGLE_FORM'})).toEqual(true);
+  });
+
+  test('should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, {type:null})).toEqual(false);
   });
 })
