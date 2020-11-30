16e18b33e929ac8b434a610f164705c72253d379
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 11:28:16 -0800

    Add tests for root reducer to check initial state of both combined reducers are working

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,6 +1,19 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
+  test('Check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+  
 test('should return defauly state if no action type is recognized', () => {
     expect(rootReducer({}, {type:null})).toEqual({
       masterTicketList: {},
