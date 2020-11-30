dbb66b1c070e894b717b866793708ef50e0b1172
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 11:30:26 -0800

    Add root reducer tests to ensure actions passed into combined reducers are reflected in root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -6,6 +6,13 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
+  test('should return defauly state if no action type is recognized', () => {
+    expect(rootReducer({}, {type:null})).toEqual({
+      masterTicketList: {},
+      formVisibleOnPage: false
+    });
+  });
+
   test('Check that initial state of ticketListReducer matches root reducer', () => {
     expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
   });
@@ -13,11 +20,26 @@ describe('rootReducer', () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
-  
-test('should return defauly state if no action type is recognized', () => {
-    expect(rootReducer({}, {type:null})).toEqual({
-      masterTicketList: {},
-      formVisibleOnPage: false
-    });
+
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
   });
+  
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+    
+  });
+
 })
