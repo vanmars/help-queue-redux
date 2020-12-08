2a32740d20ee9d25810ba11328515f6b2680a45a
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-07 21:24:20 -0800

    Add react router library and add sigin page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(){
   return ( 
-    <React.Fragment>
+    <Router>
       <Header />
-      <TicketControl />
-    </React.Fragment>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <TicketControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
